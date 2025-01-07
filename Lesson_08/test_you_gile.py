135493cafacf8488e250b6f4ca1d6c5757cc0d1f
anastasia1924 <anastasiastasia82@gmail.com>
2025-01-07 09:08:41 +0400

    Урок 8

--- Lesson_08/test_you_gile.py
+++ Lesson_08/test_you_gile.py
@@ -2,26 +2,84 @@ import requests
 
 base_url = "https://yougile.com/api-v2"
 headers = {
-    'Authorization' = 'Bearer r5E7QPvxBg1AJ+uzBA-MJyyKKQn-Ll-LHwDzHnYrH4TR-fLLLQjUR4KBVh-Li2NW'
+    'Authorization' : 'Bearer r5E7QPvxBg1AJ+uzBA-MJyyKKQn-Ll-LHwDzHnYrH4TR-fLLLQjUR4KBVh-Li2NW'
 }
-def get_projects_list(params_to_add = None):
-    resp = requests.get(base_url + '/projects', params_to_add)
-    return resp.json()
 
-
-def get_token(user = 'Anastasiastasia82@gmail.com', password = 'Ycheba2024*', companyId = 'a00ef800-2610-4b8e-bbe8-00d7b4a4300f'):
-    creds = {
-        'username': user,
-        'password': password,
-        'companyId': companyId
-    }
-    # авторизация
-    resp = requests.post(base_url + '/auth/keys', json=creds)
-    return resp.json()["key"]
-
-def test_projects(): #Получение списка всех компаний
+def test_get_projects(): #Получение списка всех компаний
     # Получить список всех компаний
-    resp = requests.get(base_url +'/projects')
+    resp = requests.get(base_url + '/projects', headers=headers)
     body = resp.json()
     assert resp.status_code == 200
     assert len(body) > 0
+
+def test_get_projects_negative(): #Получение списка всех компаний
+    # Получить список всех компаний
+    resp = requests.get(base_url + '/projects')
+    body = resp.json()
+    assert resp.status_code == 401
+
+def test_create_projects(): #Получение списка всех компаний
+    # Получить список всех компаний
+    body = {
+  "title": "Проект к дз 4"
+}
+    resp = requests.post(base_url + '/projects', json=body, headers=headers)
+    body = resp.json()
+    assert resp.status_code == 201
+
+def test_create_projects_negative(): #Получение списка всех компаний
+    # Получить список всех компаний
+    body = {
+  "title": "Проект к дз 4"
+}
+    resp = requests.post(base_url + '/projects', headers=headers)
+    body = resp.json()
+    assert resp.status_code == 400
+
+def test_get_id(): #Получение списка всех компаний
+    # Получить список всех компаний
+    body = {
+  "title": "Проект к дз 4"
+}
+    resp = requests.post(base_url + '/projects', json=body, headers=headers)
+    body = resp.json()
+    resp2 = requests.get(base_url + '/projects/'+ body['id'], headers=headers)
+    assert resp2.status_code == 200
+
+def test_get_id_negative(): #Получение списка всех компаний
+    # Получить список всех компаний
+    body = {
+  "title": "Проект к дз 4"
+}
+    resp = requests.post(base_url + '/projects', json=body, headers=headers)
+    body = resp.json()
+    resp2 = requests.get(base_url + '/projects'+ body['id'], headers=headers)
+    assert resp2.status_code == 404
+
+
+def test_change_title(): #Получение списка всех компаний
+    # Получить список всех компаний
+    body = {
+  "title": "Проект к дз 4"
+}
+    resp = requests.post(base_url + '/projects', json=body, headers=headers)
+    body = resp.json()
+    change_body = {
+        "title": "Проект к дз 5"
+    }
+    resp2 = requests.put(base_url + '/projects/'+ body['id'], json=change_body, headers=headers)
+    assert resp2.status_code == 200
+
+def test_change_title_negative(): #Получение списка всех компаний
+    # Получить список всех компаний
+    body = {
+  "title": "Проект к дз 4"
+}
+    resp = requests.post(base_url + '/projects', json=body, headers=headers)
+    body = resp.json()
+    change_body = {
+        "title": "Проект к дз 5"
+    }
+    resp2 = requests.put(base_url + '/projects/'+ body['id'], json=body, headers=headers)
+    assert resp2.status_code == 400
+
